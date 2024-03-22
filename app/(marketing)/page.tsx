6e88c3cda2b8689118295af34ccdd475110c64fd
hiroshi-imaizumi <hiroshi.imaizumi@asia-quest.jp>
2024-03-22 19:37:21 +0900

    footer

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <h1 className="text-xl lg:text-3xl font-bold text-natural-600 max-w-[480px] text-center">
           Learn, practice, and apply your skills with Duo. Get started today!
         </h1>
-        <div>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
           <ClerkLoading>
             <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
           </ClerkLoading>
