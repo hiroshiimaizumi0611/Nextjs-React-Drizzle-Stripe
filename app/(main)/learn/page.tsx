ed44aae7a7c129aec9caa8632c9ca26f93e0fd65
hiroshi-imaizumi <hiroshi.imaizumi@asia-quest.jp>
2024-03-26 22:50:08 +0900

    learn page wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,5 +1,19 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+
 const LearnPages = () => {
-  return <div>Learn</div>;
+  return (
+    <div className="flex gap-[48px] px-6">
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
+      <StickyWrapper>
+        <UserProgress activeCourse={{ title: "Spanish", imageSrc: "/es.svg"}} hearts={5} points={100} hasActiveSubscription={false} />
+      </StickyWrapper>
+    </div>
+  );
 };
 
 export default LearnPages;
