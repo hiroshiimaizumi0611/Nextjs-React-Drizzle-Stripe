1c32627d801b67ac3e425b0f67530bd848e11989
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-03-30 01:19:50 +0900

    userProgress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,15 +2,28 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-const LearnPages = () => {
+const LearnPages = async () => {
+  const userProgress = await getUserProgress();
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex gap-[48px] px-6">
       <FeedWrapper>
         <Header title="Spanish" />
       </FeedWrapper>
       <StickyWrapper>
-        <UserProgress activeCourse={{ title: "Spanish", imageSrc: "/es.svg"}} hearts={5} points={100} hasActiveSubscription={false} />
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
       </StickyWrapper>
     </div>
   );
