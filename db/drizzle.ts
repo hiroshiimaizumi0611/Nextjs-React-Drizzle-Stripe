1c58d64fd8fefc859622ee58362a39506f065981
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-03-30 19:26:26 +0900

    seed

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -3,7 +3,6 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
 
-console.log(process.env.DATABASE_URL);
 const sql = neon(process.env.DATABASE_URL!);
 const db = drizzle(sql, { schema });
 
